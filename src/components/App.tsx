9b598cad8cb8e4b3b949275b21e2810dbfd2db0b
Matthew Francis <mattfrancis888@gmail.com>
2020-11-09 21:59:50 -0500

    Set up for client side.

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Router } from "react-router-dom";
+import history from "../browserHistory";
+import Routes from "./Routes";
+const App: React.FC<{}> = () => {
+    return (
+        <Router history={history}>
+            <Routes />
+        </Router>
+    );
+};
+
+export default App;
