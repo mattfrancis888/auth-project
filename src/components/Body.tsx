f7cd53775b32698989286d90212a5e2e9ff52867
Matthew Francis <mattfrancis888@gmail.com>
2020-11-10 21:24:10 -0500

    Fixed background's responsiveness. Created 'Walkman info' box.

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -7,12 +7,39 @@ const Body: React.FC<{}> = () => {
     const { width } = useWindowDimensions();
 
     return (
-        <div>
-            <img
-                className="bodyBackgroundImg"
-                src={width > MED_SCREEN_SIZE ? background : mobileBackground}
-                alt="guardians of the galaxy"
-            />
+        <div className="bodyContainer">
+            <div className="contentContainer">
+                <div className="listenContainer">
+                    <h1 className="listenTitle">
+                        Log In To Listen to Peter Quill's Walkman
+                    </h1>
+                    <div className="listenAboutWrap">
+                        <p>
+                            Peter Quill's Walkman was an object owned by Peter
+                            Quill, given to him by his mother as a gift for her
+                            fatherless, lonely son. He became very protective of
+                            his Walkman, as it was the only thing Peter had left
+                            of his mom. It also came with a headset.
+                        </p>
+                        <div className="walkmanContainer">
+                            <img
+                                src="https://media1.giphy.com/media/YJGcmZMP7ljIA/giphy.gif"
+                                alt="walkman"
+                            ></img>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="bodyBackgroundImgWrap">
+                <img
+                    className="bodyBackgroundImg"
+                    src={
+                        width > MED_SCREEN_SIZE ? background : mobileBackground
+                    }
+                    alt="guardians of the galaxy"
+                />
+            </div>
         </div>
     );
 };
